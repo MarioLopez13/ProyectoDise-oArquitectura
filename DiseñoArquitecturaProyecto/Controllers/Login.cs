a0477bdf7a26efaccc2216f748e68cea2225a121
alex rengifo <alexren567@outlook.com>
2025-01-21 03:00:32 -0500

    Creacion de Base de Datos,Conexion a base de datos,Procedimiento almacenado para el registro y validacion de cliente,creacion de modelo y controlador para base de datos #Revisar Comentarios en ProyectContext.cs

---
+++ DiseñoArquitecturaProyecto/Controllers/Login.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DiseñoArquitecturaProyecto.Models.DB;
+namespace DiseñoArquitecturaProyecto.Controllers
+{
+    public class Login : Controller
+    {
+        //conexion base de datos
+        static string conn = "Data Source=CASAALEX;initial catalog=Proyect;Integrated Security=True";
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        /*[HttpPost]
+        public IActionResult Registrar(Cliente newCliente)
+        {
+            bool registrado;
+            string mensaje;
+            if (newCliente.c )
+            {
+            
+            }
+            return View();
+        }*/
+    }
+}
